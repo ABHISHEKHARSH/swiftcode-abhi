e2c9877b7e141e663edde86f94a277ff755f92e7
ABHISHEKHARSH <abhiharsh1995@gmail.com>
2017-10-08 09:45:23 +0000

    finally working bot

--- app/services/NewsAgentService.java
+++ app/services/NewsAgentService.java
@@ -18,11 +18,11 @@ public class NewsAgentService {
             WSRequest queryRequest = WS.url("https://api.api.ai/api/query");
             CompletionStage<WSResponse> responsePromise = queryRequest
                     .setQueryParameter("v", "20150910")
-                    .setQueryParameter("query", "keyword")
+                    .setQueryParameter("query", keyword)
                     .setQueryParameter("lang", "en")
                     .setQueryParameter("sessionId", sessionId.toString())
                     .setQueryParameter("timezone", "2017-10-07T09:23:28.257Z")
-                    .setQueryParameter("Authorization", "Bearer 946df4ead6524dbcaeb5c6c2409462b6")
+                    .setHeader("Authorization", "Bearer 946df4ead6524dbcaeb5c6c2409462b6")
                     .get();
             JsonNode response = responsePromise.thenApply(WSResponse::asJson).toCompletableFuture().get();
 
